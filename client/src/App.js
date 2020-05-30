051ec7b84e1bdcf8231738382fba822df73d908f
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2020-05-30 20:57:20 +0300

    added toggleTodo function, state, style for checked items

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,29 @@ import TodoList from './todo/TodoList'
 
 
 function App() {
-const todos = [
-  {id:1, completed: false, title:'Купить хлеб'},
-  {id:2, completed: false, title:'Купить масло'},
-  {id:3, completed: false, title:'Купить молоко'}
-]
+  const [todos, setTodos] = React.useState([
+    {id:1, completed: false, title:'Купить хлеб'},
+    {id:2, completed: false, title:'Купить масло'},
+    {id:3, completed: false, title:'Купить молоко'}
+  ])
 
 
+function toggleTodo(id){
+  setTodos(
+    todos.map(todo => {
+    if (todo.id === id) {
+      todo.completed = !todo.completed
+    }
+    return todo
+  })
+  )
+}
 
   return (
     <div className='wrapper'>
       <h1>To do list</h1>
 
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onToggle={toggleTodo} />
 	</div>
   );
 }
--- client/src/todo/TodoItem.js
+++ client/src/todo/TodoItem.js
@@ -16,10 +16,21 @@ const styles = {
     }
 }
 
-function TodoItem({todo, index }){
+function TodoItem({todo, index, onChange}){
+    console.log('todo', todo)
+    const classes = []
+
+    if(todo.completed){
+        classes.push('done')
+    }
 return <li style={styles.li}>
-    <span>
-        <input  type="checkbox" style={styles.input} onChange={() => console.log(todo.id)} />
+    <span className={classes.join(' ')}>
+        <input  
+        type="checkbox"
+        checked={todo.completed} 
+        style={styles.input} 
+        onChange={() => onChange(todo.id)} 
+        />
         <strong>{ index + 1}</strong>
         &nbsp;
         { todo.title }
@@ -31,7 +42,8 @@ return <li style={styles.li}>
 
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired,
-    index: PropTypes.number
+    index: PropTypes.number,
+    onChange: PropTypes.func.isRequired
 }
 
 export default TodoItem
