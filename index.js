127503195194c23465378beb269fe8da6aac1268
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2020-05-30 11:48:45 +0300

    removing some initial react app code and adding div with class 'wrapper'

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  //.get('/', getFoodStorageItems)
+  
   
   // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
@@ -23,28 +23,6 @@ app
   
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   
-  
-  /*
-  function getFoodStorageItems (req, res) {
-	  pool.query('SELECT itemname, amount FROM items', function(err, result) {
-      if (err) {
-        return console.error('error running query', err);
-      }
-	  // Log this to the console for debugging purposes.
-    console.log("Back from DB with result:");
-	console.log(result.rows);
-	const items = result.rows;
-	console.log("items variable:");
-	console.log(items);
-	
-	res.render('pages/index', {
-        items: items
-    });
-	
-	//callback(null, result.rows);
-	  });
-  }
-  */
   
   
   
