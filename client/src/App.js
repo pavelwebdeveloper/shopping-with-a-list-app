295cce61c2a8e430608cdabcda01ac4166edbe60
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2020-05-30 17:06:54 +0300

    adding a list, list items with checkboxes and delete buttons, creating TodoList element, ListItem element, adding styles, creating an array of objects for outputing some items, adding a type verifying library and verification for props

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,19 @@ import TodoList from './todo/TodoList'
 
 
 function App() {
+const todos = [
+  {id:1, completed: false, title:'Купить хлеб'},
+  {id:2, completed: false, title:'Купить масло'},
+  {id:3, completed: false, title:'Купить молоко'}
+]
+
+
+
   return (
     <div className='wrapper'>
-      
-      <TodoList />
+      <h1>To do list</h1>
+
+      <TodoList todos={todos} />
 	</div>
   );
 }
